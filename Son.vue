103b1a17c783752c9c863de474b2c8e9895bd451
vmax-js <771835525@qq.com>
2020-09-23 16:32:41 +0800

    单个vue

---
+++ Son.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>hhha i am a p </p>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+p{
+    color: aqua
+}
+</style>
---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        xxx
+        {{ msg }}
+        <base-son/>
+    </div>
+</template>
+
+<script>
+import baseSon from './Son';
+export default {
+    components:{
+        baseSon
+    },
+    data(){
+        return {
+            msg: "我是爸爸"
+        }
+    }
+}
+</script>
+
+<style scoped>
+div{
+    color: red;
+}
+</style>
